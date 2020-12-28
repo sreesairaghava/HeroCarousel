e93fc3cdcfa5e33137aa65edae1051f97dd32bfa
Sree Sai Raghava <raghava.rym@gmail.com>
2020-12-28 12:56:05 +0530

    Initial Commit

---
+++ HeroCarousel/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HeroCarousel
+//
+//  Created by Sree Sai Raghava Dandu on 28/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HeroCarousel/HeroCarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeroCarouselApp.swift
+//  HeroCarousel
+//
+//  Created by Sree Sai Raghava Dandu on 28/12/20.
+//
+
+import SwiftUI
+
+@main
+struct HeroCarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
